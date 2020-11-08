712f23fa1b3d52a7b62cbcd183d26b422695209c
Cameron-Stock <clstock33@gmail.com>
2020-11-08 14:01:12 -0600

    Class setup

---
+++ ClassesExample/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ClassesExample
+{
+    public class Car
+    {
+        // Option 1 (Dot notation)
+        public Car()
+        {
+        }
+
+
+        // Option 2 (Object I)
+        public Car(string makeInput, string modelInput, int year)
+        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = year;
+        }
+
+        // get = read   set = write
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ ClassesExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           // Dot Notation
+
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+
+            // Object Init
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { myCar, mazda, chevy };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+
+                 
+
+            }
+
+
+
+
+
+        }
+
+    }
+}
